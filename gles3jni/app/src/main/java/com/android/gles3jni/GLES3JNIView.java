0b9dc44431ba6da59d1289180867a055b99d7ed5
lbb <binbin.liu@plaso.me>
2022-10-30 22:53:46 +0800

    feat: add gles3jni

---
+++ gles3jni/app/src/main/java/com/android/gles3jni/GLES3JNIView.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gles3jni;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.opengles.GL10;
+
+class GLES3JNIView extends GLSurfaceView implements View.OnTouchListener{
+    private boolean isCanTouch = true;
+    /**
+     * 当前触摸的点数
+     */
+    private int pointNum = 0;
+    //最大的缩放比例
+    public static final float SCALE_MAX = 8.0f;
+    private static final float SCALE_MIN = 1.0f;
+
+    private double oldDist = 0;
+    private double moveDist = 0;
+    /**
+     * 针对控件的坐标系，即控件左上角为原点
+     */
+    private double moveX = 0;
+    private double moveY = 0;
+
+    private double downX = 0;
+    private double downY = 0;
+    // 针对屏幕的坐标系，即屏幕左上角为原点
+    private double moveRawX = 0;
+    private double moveRawY = 0;
+
+    private static final String TAG = "GLES3JNI";
+    private static final boolean DEBUG = true;
+
+    public GLES3JNIView(Context context) {
+        super(context);
+        // Pick an EGLConfig with RGB8 color, 16-bit depth, no stencil,
+        // supporting OpenGL ES 2.0 or later backwards-compatible versions.
+        setEGLConfigChooser(8, 8, 8, 0, 16, 0);
+        setEGLContextClientVersion(3);
+        setRenderer(new Renderer());
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        if (!isCanTouch) {
+            return false;
+        }
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                pointNum = 1;
+                downX = event.getX();
+                downY = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                pointNum = 0;
+                downX = 0;
+                downY = 0;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (pointNum == 1) {
+                    //只有一个手指的时候才有移动的操作
+                    float lessX = (float) (downX - event.getX());
+                    float lessY = (float) (downY - event.getY());
+                    moveX = event.getX();
+                    moveY = event.getY();
+                    moveRawX = event.getRawX();
+                    moveRawY = event.getRawY();
+                    setSelfPivot(lessX, lessY);
+                    //setPivot(getPivotX() + lessX, getPivotY() + lessY);
+                } else if (pointNum == 2) {
+                    //只有2个手指的时候才有放大缩小的操作
+                    moveDist = spacing(event);
+                    double space = moveDist - oldDist;
+                    float scale = (float) (getScaleX() + space / getWidth());
+                    if (scale > SCALE_MIN && scale < SCALE_MAX) {
+                        setScale(scale);
+                    } else if (scale < SCALE_MIN) {
+                        setScale(SCALE_MIN);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                //两点按下时的距离
+                oldDist = spacing(event);
+                pointNum += 1;
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                pointNum -= 1;
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * 触摸使用的移动事件
+     *
+     * @param lessX x坐标
+     * @param lessY y坐标
+     */
+    private void setSelfPivot(float lessX, float lessY) {
+        float setPivotX = 0;
+        float setPivotY = 0;
+        setPivotX = getPivotX() + lessX;
+        setPivotY = getPivotY() + lessY;
+        Log.e(TAG, "setPivotX:" + setPivotX + "  setPivotY:" + setPivotY
+                + "  getWidth:" + getWidth() + "  getHeight:" + getHeight());
+        if (setPivotX < 0 && setPivotY < 0) {
+            setPivotX = 0;
+            setPivotY = 0;
+        } else if (setPivotX > 0 && setPivotY < 0) {
+            setPivotY = 0;
+            if (setPivotX > getWidth()) {
+                setPivotX = getWidth();
+            }
+        } else if (setPivotX < 0 && setPivotY > 0) {
+            setPivotX = 0;
+            if (setPivotY > getHeight()) {
+                setPivotY = getHeight();
+            }
+        } else {
+            if (setPivotX > getWidth()) {
+                setPivotX = getWidth();
+            }
+            if (setPivotY > getHeight()) {
+                setPivotY = getHeight();
+            }
+        }
+        setPivot(setPivotX, setPivotY);
+    }
+
+    /**
+     * 计算两个点的距离
+     *
+     * @param event 事件
+     * @return 返回数值
+     */
+    private double spacing(MotionEvent event) {
+        if (event.getPointerCount() == 2) {
+            float x = event.getX(0) - event.getX(1);
+            float y = event.getY(0) - event.getY(1);
+            return Math.sqrt(x * x + y * y);
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * 平移画面，当画面的宽或高大于屏幕宽高时，调用此方法进行平移
+     *
+     * @param x 坐标x
+     * @param y 坐标y
+     */
+    public void setPivot(float x, float y) {
+        setPivotX(x);
+        setPivotY(y);
+    }
+
+    /**
+     * 设置放大缩小
+     *
+     * @param scale 缩放值
+     */
+    public void setScale(float scale) {
+        Log.e(TAG, "setScale : " + scale);
+        setScaleX(scale);
+        setScaleY(scale);
+    }
+
+    /**
+     * 初始化比例，也就是原始比例
+     */
+    public void setInitScale() {
+        setScaleX(1.0f);
+        setScaleY(1.0f);
+        setPivot(getWidth() / 2f, getHeight() / 2f);
+    }
+
+    private static class Renderer implements GLSurfaceView.Renderer {
+        public void onDrawFrame(GL10 gl) {
+            GLES3JNILib.step();
+        }
+
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            GLES3JNILib.resize(width, height);
+        }
+
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            GLES3JNILib.init();
+        }
+    }
+}
